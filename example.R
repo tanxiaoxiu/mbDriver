46b87619f9f31f15d8cde6b30d12b810fbf16eb0
tanxiaoxiu <tanxiaoxiu18@163.com>
2024-07-12 11:40:41 +0800

    Update

--- example.R
+++ example.R
@@ -1,65 +1,10 @@
-rm(list = ls(all = TRUE)) 
+library(mbDriver)
 library(MASS)
 library(glmnet)
 library(Rfast)
 library(mgcv)
 library(dplyr)
 library(pheatmap)
-
-###calculate f
-f <- function(X){ 
-  Y<-c()
-  X <- as.data.frame(X)
-  fmla <- formula("X~s(t, m=2, k=5,bs = 'cr')")
-  dat <- cbind(X,t)
-  mod_nb <- gam(fmla, data = dat, family = nb(), method="REML")
-  Y <- fitted.values(mod_nb)
-  return(Y)
-} 
-
-###calculate f deriv
-f_deriv <- function(X){
-  Y<-c()
-  X <- as.data.frame(X)
-  fmla <- formula("X~s(t, m=2, k=5,bs = 'cr')")
-  dat <- cbind(X,t)
-  mod_nb <- gam(fmla, data = dat, family = nb(), method="REML")
-  fitted.values <- fitted.values(mod_nb)
-  newdat <- as.data.frame(t)
-  X0 <- predict(mod_nb,newdata = newdat,type="lpmatrix")
-  eps <- 1e-7 
-  t <- t + eps 
-  newdat <- as.data.frame(t)
-  X1 <- predict(mod_nb,newdata = newdat,type="lpmatrix")
-  Xp <- (X1-X0)/eps
-  df <- Xp %*% coef(mod_nb)
-  Y <- fitted.values * df
-  return(Y)
-}
-
-###calculate D3 Score
-Driver_Score <- function(A,r){
-  x_star <- -solve(A) %*% r
-  p <- length(r)
-  D_square <- as.data.frame(matrix(nrow=p,ncol=1))
-  for (i in 1:p){
-    Ai <- A
-    Ai[-i,i] <- 0
-    ri <- r
-    z_star <- -solve(Ai) %*% ri
-    di <- x_star - z_star
-    D_square[i,] <- sum(as.numeric(di*di))
-  }
-  rownames(D_square) <- row.names(A)
-  D_square <- cbind(rownames(D_square),D_square)
-  D_square_sort <-D_square[order(-D_square[,2]),]
-  return(D_square_sort)
-}
-
-### write rownames of data
-adjustdata <- function(data) {
-  data<-cbind(rownames(data),data)
-}
 
 setwd("~/mbDriver/example")
 
@@ -73,7 +18,7 @@ data_abundance_sort <- data_abundance[,order(-as.numeric(data_abundance["Total",
 meta_abundance <- merge(metadata,data_abundance_sort,by = 'row.names', all = F)
 
 
-p = 10 
+p = 10
 n = 5
 group1 = "group1"
 group2 = "group2"
@@ -83,17 +28,17 @@ group2 = "group2"
 F_d <- data.frame()
 F_dt <- data.frame()
 
-for (i in 1:n){
-  subject_i <- meta_abundance[meta_abundance$Subject==i,]
-  subject_i <- subject_i[subject_i$Group == group1,]
-  subject_i <- subject_i[,-c(1,2,4)]
-  subject_i_top <- subject_i[,c(1:(p+1))]
-  otu_table <- subject_i_top[order(subject_i_top[,"Time"]),] 
-  t <- otu_table[,1]
-  s_f <- apply(otu_table[,-1],2,f)
-  s_f_deriv <- apply(otu_table[,-1],2,f_deriv)
-  F_d <- rbind(F_d,s_f)
-  F_dt <- rbind(F_dt,s_f_deriv)
+for (i in 1:n) {
+  subject_i <- meta_abundance[meta_abundance$Subject == i, ]
+  subject_i <- subject_i[subject_i$Group == group1, ]
+  subject_i <- subject_i[, -c(1, 2, 4)]
+  subject_i_top <- subject_i[, c(1:(p + 1))]
+  otu_table <- subject_i_top[order(subject_i_top$Time), ]
+  t <- otu_table[, 1]
+  s_f <- apply(otu_table[, -1], 2, function(col) f(col, t))
+  s_f_deriv <- apply(otu_table[, -1], 2, function(col) f_deriv(col, t))
+  F_d <- rbind(F_d, s_f)
+  F_dt <- rbind(F_dt, s_f_deriv)
 }
 F_d <- as.matrix(F_d)
 F_dt <- as.matrix(F_dt)
@@ -146,10 +91,10 @@ for (i in 1:n){
   subject_i <- subject_i[subject_i$Group == group2,]
   subject_i <- subject_i[,-c(1,2,4)]
   subject_i_top <- subject_i[,c(1:(p+1))]
-  otu_table <- subject_i_top[order(subject_i_top[,"Time"]),] 
+  otu_table <- subject_i_top[order(subject_i_top[,"Time"]),]
   t <- otu_table[,1]
-  s_f <- apply(otu_table[,-1],2,f)
-  s_f_deriv <- apply(otu_table[,-1],2,f_deriv)
+  s_f <- apply(otu_table[,-1],2,function(col) f(col, t))
+  s_f_deriv <- apply(otu_table[,-1],2,function(col) f_deriv(col, t))
   F_d <- rbind(F_d,s_f)
   F_dt <- rbind(F_dt,s_f_deriv)
 }
@@ -202,15 +147,20 @@ summary <- rbind(group1_driver,group2_driver)
 write.table(summary,file ="driver_summary.txt",row.names = F,col.names = T, sep = "\t",quote = F)
 
 ###plot
+library(ggplot2)
+library(tidyr)
+library(tibble)
+library(gtable)
+
 MAX_N_OTU = p
 l_tb <- summary
 l_tb %>% pivot_wider(names_from = "Group" ,values_from = "Score")
 
-w_tb <- l_tb %>% 
-  pivot_wider(id_cols = "Driver", 
-              names_from = "Group", 
+w_tb <- l_tb %>%
+  pivot_wider(id_cols = "Driver",
+              names_from = "Group",
               values_from = "Score",
-              values_fill=0) %>% 
+              values_fill=0) %>%
   mutate(across(-Driver, function(x) log10(x + 1))) %>%
   column_to_rownames("Driver")
 
